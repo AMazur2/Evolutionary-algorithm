0ce9af04f7da42a12b6a899a0b6ab9508ea4684c
AMazur2 <45732226+AMazur2@users.noreply.github.com>
2020-12-09 10:58:16 +0100

    Update ObserverFactory.py

--- src/evolutionaryAlgorithm/Observers/ObserverFactory.py
+++ src/evolutionaryAlgorithm/Observers/ObserverFactory.py
@@ -20,6 +20,12 @@ class ObserverFactory():
             return MinFitnessFunctionObserver(
                 path=os.path.join(dir_path, observer_config["Arguments"][
                     "OutputFileName"]))
-
+        elif observer_config["Type"] == "AverageAndDeviationObserver":
+            Path(workingDirectoryPath).mkdir(exist_ok=True)
+            Path(os.path.join(workingDirectoryPath, simulationName)).mkdir(exist_ok=True)
+            Path(dir_path).mkdir(exist_ok=True)
+            return AverageAndDeviationObserver(pathAverage=os.path.join(dir_path, observer_config["Arguments"][
+                "OutputFileName"][0]), pathDeviation=os.path.join(dir_path, observer_config["Arguments"][
+                "OutputFileName"][1]))
         else:
             raise NotImplementedError()
